d77cef28d931c06aadff8577ca0de513eb61389a
Lívia Izidório <liviaizidorio.li@gmail.com>
2025-08-29 12:04:43 -0300

    Começando a programar o tema escuro e o tema claro e adicionando funcionalidade de fechar a sidebar ao clicar fora dela

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const toggleBtn = document.querySelector(".sidebar-toggle");
 const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
+const tema = document.querySelector(".tema")
+const escuroClaro = document.getElementById("escuro-claro")
 
 toggleBtn.addEventListener("click", function () {
   // if (sidebar.classList.contains("show-sidebar")) {
@@ -13,4 +15,20 @@ toggleBtn.addEventListener("click", function () {
 
 closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
+});
+
+document.addEventListener("click", (e) => {
+  if (!sidebar.contains(e.target) && !toggleBtn.contains(e.target)) {
+    sidebar.classList.remove("show-sidebar");
+  }}
+);
+
+document.addEventListener("click", (e) => {
+  if (escuroClaro.src.includes("sol.png")) {
+    escuroClaro.src = "lua.png";
+    tema.style.padding = '1px';
+  } else {
+    escuroClaro.src = "sol.png";
+    tema.style.padding = '0px';
+  }
 });
